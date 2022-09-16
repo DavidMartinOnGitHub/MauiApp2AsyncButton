afb4c239df06d62ade46d359854f0ba7e2c4edf6
David Martin <davethecaver@hotmail.com>
2022-09-16 12:09:24 -0700

    add a 'Properties' button to provide diagnostics added the visual state code back in The T1 async button changes the BackgroundColor to grey. I observed that when I right click the mouse over T1 async the button changes back to the original blue. When the T1 async button has been left-clicked and the color is grey the Properties reports that the button is grey. It also reports that the button IsPressed. When using async code in the Command it seems that the state IsPressed does not get reset...

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -12,5 +12,11 @@ public partial class MainPage : ContentPage
         this.BindingContext = viewmodel;
     }
 
+    private void OnClickedProperties(object sender, EventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine($"{this.T1.Text.PadRight(10)} {this.T1.BackgroundColor.ToRgbaHex(false)} {this.T1.IsPressed}");
+        System.Diagnostics.Debug.WriteLine($"{this.T2.Text.PadRight(10)} {this.T2.BackgroundColor.ToRgbaHex(false)} {this.T2.IsPressed}");
+        
+    }
 }
 
