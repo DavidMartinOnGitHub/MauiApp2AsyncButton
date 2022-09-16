d8496174c5930b15fe58ea0f2dc1097a7a97f7d8
David Martin <davethecaver@hotmail.com>
2022-09-16 10:57:09 -0700

    initial check-in

---
+++ MauiApp2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.ViewModel
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private async Task TestSomethingAsync()
+        {
+            await Task.Delay(1);
+            //await Task.Delay(1).ConfigureAwait(false);
+            //await Task.Delay(1).ConfigureAwait(true);
+
+            System.Diagnostics.Debug.WriteLine(nameof(TestSomethingAsync));
+        }
+
+        [RelayCommand]
+        void TestMore()
+        {
+            System.Diagnostics.Debug.WriteLine(nameof(TestMore));
+        }
+    }
+}
