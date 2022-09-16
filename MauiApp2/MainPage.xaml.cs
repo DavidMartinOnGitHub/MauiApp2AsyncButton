64825fd11a85628b85204980bf90103119953b72
David Martin <davethecaver@hotmail.com>
2022-09-16 11:26:57 -0700

    tried putting buttons into Grid instead of ScrollView/VerticalStackPanel

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -12,16 +12,5 @@ public partial class MainPage : ContentPage
         this.BindingContext = viewmodel;
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
